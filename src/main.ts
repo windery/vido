a5311d005b47bc2c9678964c016df9ecd0a87dc5
huanglang <lang.huang@algento.com>
2024-07-19 16:04:46 +0800

    add vue devtool

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import devtools from '@vue/devtools';
 
-import './style.css'
+import './style.css';
 
-import './demos/ipc'
+import './demos/ipc';
 // If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
 // import './demos/node'
 
@@ -13,6 +14,9 @@ const app = createApp(App)
   .use(ElementPlus)
   .mount('#app')
   .$nextTick(() => {
-    postMessage({ payload: 'removeLoading' }, '*')
-  })
+    postMessage({ payload: 'removeLoading' }, '*');
+  });
 
+if (process.env.NODE_ENV === 'development') {
+  devtools.connect('127.0.0.1', 8098);
+}
